fe26568dae669e7853117dd0220d0f8f037c75bf
Marie Levchuk <marie.levchuk@gmail.com>
2024-09-30 11:17:10 +0300

    refactored the Card component

--- ishop0/src/components/Card/Card.jsx
+++ ishop0/src/components/Card/Card.jsx
@@ -1,23 +1,21 @@
-import React, { Fragment } from "react";
+import React from "react";
 import './Card.css';
 
 export default class Card extends React.Component{
     render(){
         return(
-            <Fragment>
-                <div className="Card">
-                    <div className="Card__img">
-                        <img src={`img/${this.props.model.url??'noimg.jpg'}`} alt="" />
-                    </div>
-                    <div className="Card__name">
-                        {this.props.model.name}
-                    </div>
-                    <div className="Card__info">
-                        <div className="Card__quantity">В наличии: {this.props.model.quantity}</div>
-                        <div className="Card__price">${this.props.model.price}</div>
-                    </div>
+            <div className="Card">
+                <div className="Card__img">
+                    <img src={`img/${this.props.model.url??'noimg.jpg'}`} alt="" />
                 </div>
-            </Fragment>
+                <div className="Card__name">
+                    {this.props.model.name}
+                </div>
+                <div className="Card__info">
+                    <div className="Card__quantity">В наличии: {this.props.model.quantity}</div>
+                    <div className="Card__price">${this.props.model.price}</div>
+                </div>
+            </div>
         );
     }
 }
