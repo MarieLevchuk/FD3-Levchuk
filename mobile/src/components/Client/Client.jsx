05fae089087c7d5bfbaebf12f8fdad42737a2121
Marie Levchuk <marie.levchuk@gmail.com>
2024-10-27 19:38:08 +0300

    added events

--- mobile/src/components/Client/Client.jsx
+++ mobile/src/components/Client/Client.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import './Client.css';
 
+import clientsEvents from '../../events/clientsEvents.js';
+
 export default class Client extends React.PureComponent{
+
+    edit = (e) => {
+        clientsEvents.emit('edit', e.target.parentNode.parentNode.id);
+    }
+
+    delete = (e) => {
+        clientsEvents.emit('delete', e.target.parentNode.parentNode.id);
+    }
+
     render(){
         console.log(`client ${this.props.client.id} render()`);
+
+        const status = this.props.client.balance > 0 ? 'active' : 'blocked';
         
         return(
-            <tr className="Client">
+            <tr id={this.props.client.id} className="Client">
                 <td className="Client_firstname">{this.props.client.firstname}</td>
                 <td className="Client_lastname">{this.props.client.lastname}</td>
                 <td className="Client_balance">{this.props.client.balance}</td>
-                <td className="Client_status">{this.props.client.balance > 0 ? 'active' : 'blocked'}</td>
+                <td className={"Client_status " + status}>{status}</td>
                 <td>
                     <input type="button" name="edit" value='edit' onClick={this.edit} />
                 </td>
--- mobile/src/components/MobileCompany/MobileCompany.jsx
+++ mobile/src/components/MobileCompany/MobileCompany.jsx
@@ -1,19 +1,47 @@
 import React from "react";
 import './MobileCompany.css';
 import Client from "../Client/Client";
+import clientsEvents from '../../events/clientsEvents.js';
 
 export default class MobileCompany extends React.PureComponent{
     state = {
         clients: this.props.clients
     }
 
+    componentDidMount = () => {
+        clientsEvents.addListener('filter', this.filter);
+
+        clientsEvents.addListener('edit', this.edit);
+        clientsEvents.addListener('delete', this.delete);
+    };
+
+    componentWillUnmount = () => {
+        clientsEvents.removeListener('filter', this.filter);
+
+        clientsEvents.removeListener('edit', this.edit);
+        clientsEvents.removeListener('delete', this.delete);
+    };
+
+    filter = (e) => {
+        console.log(e.target.value);
+    }
+
+    edit = (id) => {
+        console.log(id);
+        
+    }
+
+    delete = (id) => {
+        console.log('delete'+id);
+    }
+
     render(){
         return(
             <div className="Mobile-company">
                 <div className="Mobile-company__buttons">
-                    <input type="button" value='all' />
-                    <input type="button" value='active' />
-                    <input type="button" value='blocked' />
+                    <input type="button" value='all' onClick={this.filter} />
+                    <input type="button" value='active'  onClick={this.filter} />
+                    <input type="button" value='blocked'  onClick={this.filter} />
                 </div>
                 <table>
                     <thead>
